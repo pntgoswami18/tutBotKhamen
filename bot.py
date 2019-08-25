db03c9626a0d8b50a2bf5dea9ba227f1bf6473a5
Punit Goswami <pntgoswami18@gmail.com>
2019-08-25 23:26:10 +0530

    added memeber join event handler

--- bot.py
+++ bot.py
@@ -16,16 +16,26 @@ async def on_ready():
 
     # a bot user can be connected to many guilds.
     # searching for the intended one in client data
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+    # guild = discord.utils.get(client.guilds, name=GUILD)
+
     # printing name of bot, the name of server(guild), and the server’s identification number
     print(
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
 
-    guildMembers = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {guildMembers}')
+    guild_members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {guild_members}')
+
+
+@client.event
+async def on_member_join(member):
+    # create a direct message channel
+    await member.create_dm()
+    # send message
+    await member.dm_channel.send(
+        f'Hi {member.name}! Welcome to siestaria.'
+    )
 
 client.run(TOKEN)
