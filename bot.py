f12c728b10069fac1b8af94ea80e554ef55fe649
Punit Goswami <pntgoswami18@gmail.com>
2019-08-25 19:09:25 +0530

    used client methods to print guild info

--- bot.py
+++ bot.py
@@ -2,8 +2,10 @@ import os
 import discord
 from dotenv import load_dotenv
 
+# pull values frm .env file
 load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
@@ -12,4 +14,18 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
-client.run(token)
+    # a bot user can be connected to many guilds.
+    # searching for the intended one in client data
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+    # printing name of bot, the name of server(guild), and the server’s identification number
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    guildMembers = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {guildMembers}')
+
+client.run(TOKEN)
