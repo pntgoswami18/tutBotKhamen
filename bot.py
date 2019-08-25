60cb4f2c376f2ff7604be39e15101d1325f6406a
Punit Goswami <pntgoswami18@gmail.com>
2019-08-26 00:00:39 +0530

    added error handling

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import random
 from dotenv import load_dotenv
 
 # pull values frm .env file
@@ -17,6 +18,7 @@ async def on_ready():
     # a bot user can be connected to many guilds.
     # searching for the intended one in client data
     guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+    # could alternatively use the get method from discord utils
     # guild = discord.utils.get(client.guilds, name=GUILD)
 
     # printing name of bot, the name of server(guild), and the server’s identification number
@@ -29,6 +31,7 @@ async def on_ready():
     print(f'Guild Members:\n - {guild_members}')
 
 
+# handling event of a new memeber joining the guild
 @client.event
 async def on_member_join(member):
     # create a direct message channel
@@ -37,5 +40,42 @@ async def on_member_join(member):
     await member.dm_channel.send(
         f'Hi {member.name}! Welcome to siestaria.'
     )
+
+# handling custom messages being posted in the guild
+@client.event
+async def on_message(message):
+
+    # don't want my bot to reply to my own messages
+    # or its own messages in the guild
+    if message.author is client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content is '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+
+    if 'happy birthday' in message.content.lower():
+        await message.channel.send('Happy Birthday! 🎈🎉')
+
+    #  deliberately raise exception
+    if message.content is 'raise-exception':
+        raise discord.DiscordException
+
+
+async def on_error(event, *a, **k):
+    with open('err.log', a) as f:
+        if event is 'on_message':
+            f.write(f'Unhandled message: {a[0]}\n')
+        else:
+            raise
 
 client.run(TOKEN)
